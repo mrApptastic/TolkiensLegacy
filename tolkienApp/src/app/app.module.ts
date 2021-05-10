b43ff22beb64555bd8cb1fb3bb3f2979ad20dbeb
mrApptastic <30831839+mrApptastic@users.noreply.github.com>
2021-05-10 15:06:42 +0200

    TolkienApp: Cleaner code and stuff.

--- tolkienApp/src/app/app.module.ts
+++ tolkienApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +14,7 @@ import { CharacterGeneratorModule } from './character-generator/character-genera
     AppRoutingModule,
     CharacterGeneratorModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
