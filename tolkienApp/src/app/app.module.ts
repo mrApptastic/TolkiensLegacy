3a325d641424551943567b9464e6b6b43c5378c0
mrApptastic <30831839+mrApptastic@users.noreply.github.com>
2021-05-11 22:15:55 +0200

    TolkienApp: Fun with packages.

--- tolkienApp/src/app/app.module.ts
+++ tolkienApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterGeneratorModule } from './character-generator/character-generator.module';
+import { MrMr2Module } from 'mr-mr2';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CharacterGeneratorModule } from './character-generator/character-genera
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CharacterGeneratorModule
+    CharacterGeneratorModule,
+    MrMr2Module
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
